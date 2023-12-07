5d16adab30acbaa55c7408f708194d66f3477b99
erenJaegerMeister <marcelliusfelixmatius@gmail.com>
2023-12-07 14:31:27 +0700

    backend upload data

---
+++ public/home.js
@@ -0,0 +1,10 @@
+const btnPeople = document.querySelector("#btnPeople");
+const tablePeople = document.querySelector(".tabelPeople");
+
+btnPeople.addEventListener("click", () => {
+  if (tablePeople.style.display == "block") {
+    tablePeople.style.display = "none";
+  } else {
+    tablePeople.style.display = "block";
+  }
+});
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const db = require("./db");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
 
@@ -14,6 +16,47 @@ app.get("/", async (req, res) => {
   } catch (error) {
     console.log("error execute query: ", error);
     res.status(500).send("internal server eror");
+  }
+});
+
+app.post("/postPeopleTable", async (req, res) => {
+  try {
+    // Extract data from the form
+    const {
+      IDPeople,
+      YearBirthPeople,
+      peopleEducation,
+      peopleMaritalStatus,
+      incomePeople,
+      kidHomePeople,
+      teenHomePeople,
+      dateCustPeople,
+      recencyPeople,
+      complainPeople,
+    } = req.body;
+
+    // Insert data into the database
+    await db.query(
+      "INSERT INTO people (ID, Year_Birth, Education, Marital_Status, Income, Kidhome, Teenhome, Dt_Customer, Recency, Complain) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        IDPeople,
+        YearBirthPeople,
+        peopleEducation,
+        peopleMaritalStatus,
+        incomePeople,
+        kidHomePeople,
+        teenHomePeople,
+        dateCustPeople,
+        recencyPeople,
+        complainPeople,
+      ]
+    );
+
+    // Redirect to the home page after successful submission
+    res.redirect("/");
+  } catch (error) {
+    console.log("Error executing query: ", error);
+    res.status(500).send("Internal server error");
   }
 });
 
