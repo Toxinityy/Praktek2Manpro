6d953f15a9f0b3df632cb2c4021796dd9b230d36
William Arthur <101073828+Toxinityy@users.noreply.github.com>
2024-01-02 19:01:36 +0700

    Minor fix bug

--- public/js/script.js
+++ public/js/script.js
@@ -11,23 +11,20 @@
 $(document).ready(function () {
     $('#myTable').DataTable({
       ajax: {
-        url: '/api/getData', // Replace with your API endpoint to fetch data
-        dataSrc: '', // This is required to correctly parse the JSON data
+        url: '/api/getData',
+        dataSrc: '',
       },
       columns: [
         { data: 'ID' },
         { data: 'Year_Birth' },
         { data: 'Education' },
-        // Add more columns as needed
       ],
     });
-  });// Add this route to your server file (e.g., app.js or index.js)
+  });
 app.get('/api/getData', async (req, res) => {
   try {
-    // Fetch data from your database
     const data = await fetchDataFromDatabase();
 
-    // Send the data as JSON
     res.json(data);
   } catch (error) {
     console.error('Error fetching data:', error);
